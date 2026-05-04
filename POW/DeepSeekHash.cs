0babffd7504548c96762d40e2fcccc114d18b8b0
Nerx <nikitahysnutdinov@gmail.com>
2026-05-04 22:22:43 +0700

    POW солвер

---
+++ POW/DeepSeekHash.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Wasmtime;
+
+namespace DeepSeekAPI.POW;
+
+public class DeepSeekHash : IDisposable
+{
+    private readonly Engine _engine;
+    private readonly Store _store;
+    private readonly Linker _linker;
+    private readonly Instance _instance;
+    private readonly Memory _memory;
+
+    private readonly Func<int, int, int> _malloc;
+    private readonly Func<int, int> _addStack;
+    private readonly Function _wasmSolve;
+
+    public DeepSeekHash(string wasmPath)
+    {
+        _engine = new Engine();
+        _store = new Store(_engine);
+        _linker = new Linker(_engine);
+
+        _linker.DefineWasi();
+
+        var module = Module.FromFile(_engine, wasmPath);
+        _instance = _linker.Instantiate(_store, module);
+
+        _memory = _instance.GetMemory("memory")!;
+
+        _malloc = _instance.GetFunction<int, int, int>("__wbindgen_export_0")!;
+        _addStack = _instance.GetFunction<int, int>("__wbindgen_add_to_stack_pointer")!;
+        _wasmSolve = _instance.GetFunction("wasm_solve")!;
+    }
+
+    private (int ptr, int len) WriteToMemory(string text)
+    {
+        var encoded = Encoding.UTF8.GetBytes(text);
+        var length = encoded.Length;
+
+        var ptr = _malloc(length, 1);
+        var mem = _memory.GetSpan<byte>(0);
+
+        encoded.CopyTo(mem.Slice(ptr, length));
+        return (ptr, length);
+    }
+
+    public long? CalculateHash(string algorithm, string challenge, string salt,
+                              int difficulty, int expireAt)
+    {
+        var prefix = $"{salt}_{expireAt}_";
+        var retptr = _addStack(-16);
+
+        try
+        {
+            var (challengePtr, challengeLen) = WriteToMemory(challenge);
+            var (prefixPtr, prefixLen) = WriteToMemory(prefix);
+
+            _wasmSolve.Invoke(
+                retptr,
+                challengePtr,
+                challengeLen,
+                prefixPtr,
+                prefixLen,
+                (float)difficulty
+            );
+
+            var mem = _memory.GetSpan<byte>(0);
+            var status = BitConverter.ToInt32(mem.Slice(retptr, 4));
+
+            if (status == 0)
+            {
+                return null;
+            }
+
+            var value = BitConverter.ToDouble(mem.Slice(retptr + 8, 8));
+
+            return (long)value;
+        }
+        finally
+        {
+            _addStack(16);
+        }
+    }
+
+    public void Dispose()
+    {
+        _store?.Dispose();
+        _engine?.Dispose();
+    }
+}
---
+++ POW/DeepSeekPOW.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace DeepSeekAPI.POW;
+
+public class DeepSeekPOW
+{
+    private readonly DeepSeekHash _hasher;
+
+    public DeepSeekPOW(string wasmPath) => _hasher = new DeepSeekHash(wasmPath);
+
+    public string SolveChallenge(Dictionary<string, object> config)
+    {
+        long? answer = _hasher.CalculateHash(
+            (string)config["algorithm"],
+            (string)config["challenge"],
+            (string)config["salt"],
+            Convert.ToInt32(config["difficulty"]),
+            Convert.ToInt32(config["expire_at"])
+        );
+
+        var result = new Dictionary<string, object>
+        {
+            ["algorithm"] = config["algorithm"],
+            ["challenge"] = config["challenge"],
+            ["salt"] = config["salt"],
+            ["answer"] = answer!,
+            ["signature"] = config["signature"],
+            ["target_path"] = config["target_path"]
+        };
+
+        var json = JsonSerializer.Serialize(result);
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(json));
+    }
+}
